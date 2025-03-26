93203c0b23a6e63c6c94f84b38178347a46fb840
FrancianeRamos <francianeramso649@gmail.com>
2025-03-26 14:46:48 -0300

    Adicionando classe dependente

---
+++ LumenSys.WebAPI/Objects/Models/Dependent.cs
@@ -0,0 +1,10 @@
+﻿namespace LumenSys.WebAPI.Objects.Models
+{
+    public class Dependent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+
+    }
+}

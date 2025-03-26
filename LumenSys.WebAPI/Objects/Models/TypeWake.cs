5027caa82e853f17f18d18cd4a31c9e443ae4cae
FrancianeRamos <francianeramso649@gmail.com>
2025-03-26 16:44:55 -0300

    Adicionando classe Tipo Velório

---
+++ LumenSys.WebAPI/Objects/Models/TypeWake.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LumenSys.WebAPI.Objects.Models
+{
+    public class TypeWake
+    {
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+
+        public TypeWake() { }
+
+        public TypeWake(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+    }
+}

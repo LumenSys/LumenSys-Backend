ae93a2edb503173b4bf2bd11e066baf3d176778d
FrancianeRamos <francianeramso649@gmail.com>
2025-03-26 16:36:50 -0300

    Adicionando a classse velório

---
+++ LumenSys.WebAPI/Objects/Models/Wake.cs
@@ -0,0 +1,12 @@
+﻿namespace LumenSys.WebAPI.Objects.Models
+{
+    public class Wake
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public string Location { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public string Description { get; set; }
+    }
+}

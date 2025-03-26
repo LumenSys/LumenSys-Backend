682f45b05d7187dd6a1f06bcbb03bdfcb4ccb6de
FrancianeRamos <francianeramso649@gmail.com>
2025-03-26 14:55:46 -0300

    Adiconando a classe tanatopraxia

---
+++ LumenSys.WebAPI/Objects/Models/Thanatopraxia.cs
@@ -0,0 +1,10 @@
+﻿namespace LumenSys.WebAPI.Objects.Models
+{
+    public class Thanatopraxia
+    {
+        public string Id { get; set; }
+        public DateOnly Date { get; set; }
+        public string Description { get; set; }
+        public string ConditionBody { get; set; }
+    }
+}

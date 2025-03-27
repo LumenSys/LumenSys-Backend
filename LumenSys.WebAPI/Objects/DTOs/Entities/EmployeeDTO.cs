d736270bcf909452a8ebd808f7cbe0d03e7cf71b
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-03-27 09:49:55 -0300

    :sparkles: feat: Adicionando Mappings EmployeeDTO

---
+++ LumenSys.WebAPI/Objects/DTOs/Entities/EmployeeDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace LumenSys.WebAPI.Objects.Dtos
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateOnly HiringDate { get; set; }
+    }
+}
---
+++ LumenSys.WebAPI/Objects/DTOs/Mappings/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using LumenSys.WebAPI.Objects.Dtos;
+using LumenSys.WebAPI.Objects.Models;
+
+namespace LumenSys.WebAPI.Objects.DTOs.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<EmployeeDTO, Employee>().ReverseMap();
+        }
+    }
+}

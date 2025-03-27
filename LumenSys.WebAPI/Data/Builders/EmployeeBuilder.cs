e3fff8c4f988ebb5735a455ae996cad754f007bb
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-03-27 10:11:51 -0300

    :sparkles: feat: Adiconando classe employeeBuilder

---
+++ LumenSys.WebAPI/Data/Builders/EmployeeBuilder.cs
@@ -0,0 +1,35 @@
+﻿using LumenSys.WebAPI.Objects.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LumenSys.WebAPI.Objects.Data.Builders
+{
+    public class EmployeeBuilder
+    {
+        public static void Build(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasKey(e => e.Id);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Cpf)
+                .IsRequired()
+                .HasMaxLength(11);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Phone)
+                .HasMaxLength(13);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Email)
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.HirinDate)
+                .IsRequired();
+        }
+    }
+}

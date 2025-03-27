09fb7a18ce681aac48b02dbae9db0531767fdbb9
FrancianeRamos <francianeramso649@gmail.com>
2025-03-27 13:10:28 -0300

    Atualizando a classe Dependente

--- LumenSys.WebAPI/Objects/Models/Dependent.cs
+++ LumenSys.WebAPI/Objects/Models/Dependent.cs
@@ -1,10 +1,24 @@
-﻿namespace LumenSys.WebAPI.Objects.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LumenSys.WebAPI.Objects.Models
 {
+    [Table("Dependent")]
     public class Dependent
     {
+        [Column("Id")]
         public int Id { get; set; }
+        [Column("Name")]
         public string Name { get; set; }
+        [Column("Cpf")]
         public string Cpf { get; set; }
 
+        public Dependent() { }
+
+        public Dependent(int id, string name, string cpf)
+        {
+            Id = id;
+            Name = name;
+            Cpf = cpf;
+        }
     }
 }

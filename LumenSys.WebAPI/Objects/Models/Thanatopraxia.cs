8ed462f944bb9ecaca1608d3bfc8990bdf3a231f
FrancianeRamos <francianeramso649@gmail.com>
2025-03-27 13:14:47 -0300

    Atualizando a classe Tanatopraxia

--- LumenSys.WebAPI/Objects/Models/Thanatopraxia.cs
+++ LumenSys.WebAPI/Objects/Models/Thanatopraxia.cs
@@ -1,10 +1,27 @@
-﻿namespace LumenSys.WebAPI.Objects.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LumenSys.WebAPI.Objects.Models
 {
+    [Table("Thanatopraxia")]
     public class Thanatopraxia
     {
+        [Column("Id")]
         public string Id { get; set; }
+        [Column("Date")]
         public DateOnly Date { get; set; }
+        [Column("Description")]
         public string Description { get; set; }
+        [Column("ConditionBody")]
         public string ConditionBody { get; set; }
+
+        public Thanatopraxia() { }
+
+        public Thanatopraxia(string id, DateOnly date, string description, string conditionBody)
+        {
+            Id = id;
+            Date = date;
+            Description = description;
+            ConditionBody = conditionBody;
+        }
     }
 }

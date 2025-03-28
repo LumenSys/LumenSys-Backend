34b2b17f0e540168d1bb6a11c9f6cd78ac0b6aea
rodrigo-lombezzi <rodrigoylombezzi@gmail.com>
2025-03-28 16:31:52 -0300

    :sparkles: feat: Adicionado classe usuario

---
+++ LumenSys.WebAPI/Objects/Models/User.cs
@@ -0,0 +1,30 @@
+using LumenSys.Objects.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LumenSys.WebAPI.Objects;
+
+[Table("user")]
+public class User
+{
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("email")]
+    public string Email { get; set; }
+    [Column("password")]
+    public string Password { get; set; }
+    [Column("typeemployee")]
+    public TypeEmployee TypeEmployee { get; set; }
+    [Column("userstatus")]
+    public UserStats UserStatus { get; set; }
+
+    public User () { }
+
+    public User (int id, string email, string password, TypeEmployee typeemployee, UserStats userstatus) 
+    {
+        Id = id;
+        Email = email;
+        Password = password;
+        TypeEmployee = typeemployee;
+        UserStatus = userstatus;
+    }
+}

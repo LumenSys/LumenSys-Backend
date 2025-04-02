c42a3c90955d3a29a662fc6cdb1c214087449971
Edsincp <edsonpereiracarvalho10@gmail.com>
2025-04-02 10:23:14 -0300

    :sparkles: feat: Adicionando DTO FuneralPlans e ajustando mapeamento no AutoMapper

---
+++ LumenSys.WebAPI/Objects/DTOs/Entities/FuneralPlansDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace LumenSys.WebAPI.Objects.DTOs.Entities
+{
+    public class FuneralPlansDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double MonthlyValue { get; set; }
+        public int? TypePlanId { get; set; }
+    }
+}

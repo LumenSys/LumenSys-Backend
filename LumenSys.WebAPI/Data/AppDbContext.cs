8aa9d217e400cc5000e2aba52e0c5774e4a28976
Edsincp <edsonpereiracarvalho10@gmail.com>
2025-04-09 22:14:38 -0300

    :sparkles: feat: Adicionando entidade do Velório na appDbContext

--- LumenSys.WebAPI/Data/AppDbContext.cs
+++ LumenSys.WebAPI/Data/AppDbContext.cs
@@ -9,11 +9,14 @@ namespace LumenSys.WebAPI.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Employee> Employees { get; set; }
+        public DbSet<Wake> Wakes { get; set; } 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             EmployeeBuilder.Build(modelBuilder);
+            WakeBuilder.Build(modelBuilder); 
         }
     }
 }

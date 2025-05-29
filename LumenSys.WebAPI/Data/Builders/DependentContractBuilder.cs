d115bfcc2dea6e63dc59a02ecdeca03a60614c5e
Edsincp <edsonpereiracarvalho10@gmail.com>
2025-05-29 14:25:09 -0300

    :sparkles: feat: DependentContract Builder/Repositories add

---
+++ LumenSys.WebAPI/Data/Builders/DependentContractBuilder.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public static class DependentContractBuilder
+{
+    public static void Build(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DependentContract>().HasKey(d => d.Id);
+
+        modelBuilder.Entity<DependentContract>()
+            .Property(d => d.DependentName).IsRequired().HasMaxLength(100);
+
+        modelBuilder.Entity<DependentContract>()
+            .Property(d => d.Relationship).IsRequired().HasMaxLength(50);
+
+        modelBuilder.Entity<DependentContract>()
+            .Property(d => d.BirthDate).IsRequired();
+
+        modelBuilder.Entity<DependentContract>()
+            .HasOne(d => d.Contract)
+            .WithMany(c => c.Dependents)
+            .HasForeignKey(d => d.ContractId);
+    }
+}
---
+++ LumenSys.WebAPI/Data/Repositories/DependentContractRepository.cs
@@ -0,0 +1,12 @@
+﻿using LumenSys.WebAPI.Data.Repositories;
+using LumenSys.WebAPI.Data;
+
+public class DependentContractRepository : GenericRepository<DependentContract>, IDependentContractRepository
+{
+    private readonly AppDbContext _context;
+
+    public DependentContractRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}

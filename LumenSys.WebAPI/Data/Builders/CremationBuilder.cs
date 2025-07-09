63943081c37ca9d00f0389b98650f3ee2c86459e
FrancianeRamos <francianeramos649@gmail.com>
2025-07-09 18:25:11 -0300

    :construction: Implementando Api

---
+++ LumenSys.WebAPI/Data/Builders/CremationBuilder.cs
@@ -0,0 +1,26 @@
+﻿using LumenSys.WebAPI.Objects.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LumenSys.WebAPI.Data.Builders
+{
+    public class CremationBuilder
+    {
+        public static void Build(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cremation>().HasKey(c => c.Id);
+
+            modelBuilder.Entity<Cremation>()
+                .Property(c => c.Date)
+                .IsRequired();
+
+            modelBuilder.Entity<Cremation>()
+                .Property(c => c.Time)
+                .IsRequired();
+
+            modelBuilder.Entity<Cremation>()
+                .Property(c => c.Number)
+                .IsRequired();
+
+        }
+    }
+}
---
+++ LumenSys.WebAPI/Services/Entities/CremationService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using LumenSys.WebAPI.Data.Interfaces;
+using LumenSys.WebAPI.Objects.DTOs.Entities;
+using LumenSys.WebAPI.Objects.Models;
+using LumenSys.WebAPI.Services.Interfaces;
+
+namespace LumenSys.WebAPI.Services.Entities
+{
+    public class CremationService : GenericService<Cremation, CremationDTO>, ICremationService
+    {
+        private readonly ICremationRepository _cremationRepository;
+        private readonly IMapper _mapper;
+        public CremationService(ICremationRepository repository, IMapper mapper) : base(repository, mapper)
+        {
+            _cremationRepository = repository;
+            _mapper = mapper;
+        }
+        public override async Task Update(CremationDTO dto, int id)
+        {
+            if (dto == null)
+                throw new ArgumentNullException("Cremation inválida.");
+            if (dto.Id != id)
+                throw new ArgumentException("ID da cremação não corresponde.");
+            await base.Update(dto, id);
+        }
+        public override async Task<CremationDTO> GetById(int id)
+        {
+            var cremation = await _cremationRepository.GetById(id);
+            if (cremation == null)
+                throw new ArgumentNullException($"Cremation com ID {id} não foi encontrado.");
+            return _mapper.Map<CremationDTO>(cremation);
+        }
+        public override async Task Delete(int id)
+        {
+            var cremation = await _cremationRepository.GetById(id);
+            if (cremation == null)
+                throw new ArgumentNullException($"Cremation com ID {id} não foi encontrado.");
+            await base.Delete(id);
+        }
+    }
+}
